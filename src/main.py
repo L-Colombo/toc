ace1ac3dadf842734a63d817e9ceda0bf09e2c27
Lorenzo Colombo <colombo.bb.lorenzo@gmail.com>
2026-08-20 18:56:53 +0200

    feat(io): simplify cli

--- src/main.py
+++ src/main.py
@@ -72,49 +72,51 @@ def gen(
 @click.argument("path_in", required=True)
 @click.argument("toc_file", required=True)
 @click.argument("out", required=True)
+@click.option("--dry-run", "dry_run", is_flag=True)
 @click.option("--readable", "readable", is_flag=True)
-@click.option("--print-toc", "print_toc", is_flag=True)
-@click.option("--debug", "debug", is_flag=True)
 @click.option("--vpos", "vpos", is_flag=True)
-def io(path_in, toc_file, out, readable, print_toc, debug, vpos):
+def io(path_in, toc_file, out, readable, dry_run, vpos):
     """
     Read the data.toc file and write table of contents to a pdf
     """
 
     try:
         with open_pdf(path_in) as doc:
-            if print_toc:
+            if dry_run:
                 toc = read_toc(doc)
 
                 if readable:
                     print(pprint_toc(toc))
                 else:
                     print(dump_toc(toc, vpos), end="")
-                sys.exit(0)
 
-            toc = parse_toc(toc_file)
-            write_toc(doc, toc)
+            else:
+                toc = parse_toc(toc_file)
+                write_toc(doc, toc)
 
-            doc.save(out)
+                doc.save(out)
+
     except ValueError as e:
-        if debug:
-            raise e
+        raise e
+
         print("error:", e, file=sys.stderr)
         sys.exit(1)
+
     except IOError as e:
-        if debug:
-            raise e
+        raise e
+
         print("error: unable to open file", file=sys.stderr)
         print(e, file=sys.stderr)
         sys.exit(1)
+
     except IndexError as e:
-        if debug:
-            raise e
+        raise e
+
         print("index error:", e, file=sys.stderr)
         sys.exit(1)
     except KeyboardInterrupt as e:
-        if debug:
-            raise e
+        raise e
+
         print("error: interrupted", file=sys.stderr)
         sys.exit(1)
 
